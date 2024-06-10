55e10dca8b1d4352bc4ebeeedadb3277618782bf
juyeong-byeon <wndudqus@naver.com>
2024-06-10 23:51:24 +0900

    feat: deploy 3

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Test deploy 2</h1>
+        <h1>Test deploy 3</h1>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
